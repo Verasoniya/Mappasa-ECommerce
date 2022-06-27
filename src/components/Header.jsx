b78b55654a6958f0fb12d21b1f043bb492f59261
dzaki <dzakifauzi.mubarok96@gmail.com>
2022-06-27 11:20:11 +0700

    testting

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="bg-opacity-10">
+      <div></div>
+    </div>
+  );
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      green: "F2F2F0",
+    },
   },
   plugins: [],
 };
